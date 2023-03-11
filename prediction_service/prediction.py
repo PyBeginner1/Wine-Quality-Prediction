dcb80dea9f98f6faab3f5d7ce2e15b3031a231ab
NightNight1 <69844203+NightNight1@users.noreply.github.com>
2023-03-11 09:11:45 +0530

    Updated prediction service

--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -5,9 +5,15 @@ import os
 import json
 import shutil
 from pathlib import Path
+import shutil
 
 params_path = "params.yaml"
-schema_path = os.path.join('prediction', 'schema_in.json')
+schema_path = os.path.join('prediction_service', 'schema_in.json')
+
+source_file = 'notebooks\schema_in.json'
+destination_folder = schema_path
+if not os.path.exists(destination_folder):
+    shutil.copy(source_file, destination_folder)
 
 current_dir = os.getcwd()
 src_path = os.path.join(current_dir, Path('saved_models/model.joblib'))
@@ -27,10 +33,11 @@ class NotInColumn(Exception):
 
 
 def form_response(dict_request):
-    data = dict_request.values()
-    data = [list(map(float, data))]
-    response = prediction(data)
-    return response
+    if validate_input(dict_request):
+        data = dict_request.values()
+        data = [list(map(float, data))]
+        response = prediction(data)
+        return response
 
 
 def read_params(config_path):
@@ -43,7 +50,7 @@ def prediction(data):
     try:
         config = read_params(params_path)
         if not os.listdir(check_dir):
-            shutil.move(src_path, dest_path)
+            shutil.copy(src_path, dest_path)
         model_dir = config['webapp_model_dir']
         model = joblib.load(model_dir)
         prediction = model.predict(data)
@@ -69,12 +76,28 @@ def api_response(request):
         raise e
     
 
-def get_schema(schema_path):
+def get_schema(schema_path=schema_path):
     with open(schema_path) as json_file:
-        schema = json.safe_load(json_file)
+        schema = json.load(json_file)
     return schema
 
 
 def validate_input(dict_request):
-    pass
+    def _validate_cols(col):
+        schema = get_schema()
+        actual_cols = schema.keys()
+        if col not in actual_cols:
+            raise NotInColumn
+        
+    def _validate_values(col, val):
+        schema = get_schema()
+
+        if not (schema[col]["min"] <= float(dict_request[col]) <= schema[col]["max"]):
+            raise NotInRange
+        
+    for col, val in dict_request.items():
+        _validate_cols(col)
+        _validate_values(col,val)
+
+    return True
     
